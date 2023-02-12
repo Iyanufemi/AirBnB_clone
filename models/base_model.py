850e0a97db5f6d69688e7ff9daa77a4fe73c9a29
iyanufemi <iyanu.akinladenu@gmail.com>
2023-02-12 10:12:40 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Parent class that will be inherit
+Parent class that will inherit
 """
 import uuid
 from datetime import datetime
@@ -16,7 +16,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
@@ -39,7 +39,7 @@ class BaseModel:
         self.updated_at = datetime.now()
         storage.save()
 
-        def to_dict(self):
+    def to_dict(self):
         """creates a new dictionary, adding a key and returning
         datemtimes converted to strings
         """
